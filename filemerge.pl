e12316c16962f1fd602e04d7fda1e223ed666953
Steven Steele <steven.harville.steele@gmail.com>
2013-04-14 22:57:34 -0500

    Standing ground with work com switchover

--- filemerge.pl
+++ filemerge.pl
@@ -4,9 +4,9 @@ $left = $ARGV[0];
 $right = $ARGV[1];
 
 if ($left && $right) {
-	system ("/Applications/Xcode.app/Contents/Applications/FileMerge.app/Contents/MacOS/FileMerge -left $left -right $right &");
+	system ("/Developer/Applications/Utilities/FileMerge.app/Contents/MacOS/FileMerge -left $left -right $right &");
 } else {
-	system ("/Applications/Xcode.app/Contents/Applications/FileMerge.app/Contents/MacOS/FileMerge&");
+	system ("/Developer/Applications/Utilities/FileMerge.app/Contents/MacOS/FileMerge &");
 }
 
 exit
