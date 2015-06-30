85d62eb8794091e6964047004fe223998ab229af
Steve Steele <ssteele@financegenius.com>
2015-06-30 12:00:02 -0500

    Abstract out sublime text version

--- project_task.pl
+++ project_task.pl
@@ -2,7 +2,7 @@
 
 die "\nThis program requires command-line input\n\n" if (!$ARGV[0]);
 chomp ($project = $ARGV[0]);
-$command = '/Applications/Sublime\ Text\ 2.app/Contents/SharedSupport/bin/subl';
+$command = '~/ssteele/bin/subl';
 
 system ("$command -n ~/sublime_projects_tasks/$project.sublime-project");
 
