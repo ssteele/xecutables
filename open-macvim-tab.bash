5196eeedcf322fa90b8a6dbf7f90cd08756f52c1
Steve Steele <steven.harville.steele@gmail.com>
2016-08-14 17:54:38 -0500

    Associate a terminal w/ a particular vim window for `v` (open file) commands

--- open-macvim-tab.bash
+++ open-macvim-tab.bash
@@ -5,6 +5,9 @@ lastArg="${@: -1}"
 if [[ $lastArg =~ ^-?[0-9]+$ ]]; then
     fileNames=${@:1:$#-1}
     mvim --servername VIM${lastArg} --remote-tab ${fileNames}
+elif [[ "$vimwin" ]]; then
+    fileNames=${@:1}                                                # export vimwin=1
+    mvim --servername VIM${vimwin} --remote-tab ${fileNames}
 else
     fileNames="$@"
     mvim --remote-tab-silent ${fileNames}
