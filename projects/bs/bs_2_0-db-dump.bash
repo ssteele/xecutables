31bc8c468b09584f321a09d9fc043f2a96af03d1
Steve Steele <steven.harville.steele@gmail.com>
2017-08-09 12:05:24 -0500

    BS: Add DB dump/load VM scripts

---
+++ projects/bs/bs_2_0-db-dump.bash
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#     VERIFY ENVIRONMENT
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+source ${xec}/verify-bash-variables.bash
+
+# validate all variables
+verify_bash_exports gr
+bash_exports_valid=$?
+
+# validate all aliases
+verify_bash_aliases
+bash_aliases_valid=$?
+
+if [[ 0 = ${bash_exports_valid} || 0 = ${bash_aliases_valid} ]]; then
+    exit
+fi
+
+shopt -s expand_aliases
+source ~/.bashrc
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#     DUMP BS DB
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+user='root'
+password='password'
+database='bitesquad'
+
+gr
+
+echo ''
+echo '...dumping current DB'
+mysqldump -u${user} -p${password} ${database} > `date +%Y-%m-%d`_${database}.sql
+gzip `date +%Y-%m-%d`_${database}.sql
+echo ''
+echo '...now invoke `bs_db_fetch` from the host machine'
+echo ''
+
+exit
---
+++ projects/bs/bs_2_0-db-load.bash
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#     VERIFY ENVIRONMENT
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+source ${xec}/verify-bash-variables.bash
+
+# validate all variables
+verify_bash_exports gr
+bash_exports_valid=$?
+
+# validate all aliases
+verify_bash_aliases
+bash_aliases_valid=$?
+
+if [[ 0 = ${bash_exports_valid} || 0 = ${bash_aliases_valid} ]]; then
+    exit
+fi
+
+shopt -s expand_aliases
+source ~/.bashrc
+
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#     LOAD BS DB
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+user='root'
+password='password'
+database='bitesquad'
+
+gr
+
+if [[ -z "$1" ]]; then
+    echo 'Please supply the SQL input file as an argument'
+    exit
+fi
+input="$1"
+
+doDump=true
+if [[ -n "$2" ]]; then
+    if [[ 'false' = $2 ]]; then
+        doDump=false
+    fi
+fi
+
+echo ''
+
+if $doDump; then
+    echo '...dumping current DB'
+    mysqldump -u${user} -p${password} ${database} > `date +%Y-%m-%d`_${database}.sql
+    gzip `date +%Y-%m-%d`_${database}.sql
+fi
+
+echo "...loading $input"
+sql_file=${input%.gz}
+if [ "$sql_file" != "$input" ]; then
+    gunzip $input
+fi
+mysql -u${user} -p${password} -D${database} < $sql_file
+echo ''
+
+exit
