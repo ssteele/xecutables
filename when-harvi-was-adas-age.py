fabbdfd11e0cf78a09d941e00949b5c4dad53219
Steve Steele <steven.harville.steele@gmail.com>
2021-11-08 05:45:22 -0600

    Updates

---
+++ when-harvi-was-adas-age.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python
+
+from datetime import date as D
+
+adaBirthday = D(2020, 6, 8)
+adaAge = (D.today() - adaBirthday)
+
+harviBirthday = D(2016, 11, 16)
+dayHarviWasAdasAge = (harviBirthday + adaAge)
+
+print('\n    ' + str(adaAge.days) + ' days -> ' + str(dayHarviWasAdasAge) + '\n')
