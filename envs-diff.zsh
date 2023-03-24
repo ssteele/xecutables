569cfd5bcaec9b3f41bc06d5378f1c0839b8629f
Steve Steele <steven.harville.steele@gmail.com>
2023-03-24 11:59:23 -0500

    Encourage updates

--- envs-diff.zsh
+++ envs-diff.zsh
@@ -19,7 +19,13 @@ fi
 
 envFile=''
 if [[ -z "$1" ]]; then
-    envFile='_env.bash'                                             # @todo: _env.zsh
+    envFile='_env.zsh'
+    if [ ! -f ./_env.zsh ]; then
+        echo ''
+        echo 'mv _env.bash _env.zsh'
+        echo ''
+        envFile='_env.bash'
+    fi
 else
     envFile="$1"
 fi
