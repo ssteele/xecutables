4d01d5d8695eb298225703810d72367a6d8248eb
Steve Steele <steven.harville.steele@gmail.com>
2023-10-27 14:26:35 -0500

    Updates

---
+++ jira-ticket-markdown.py
@@ -0,0 +1,26 @@
+#!/usr/bin/python3
+
+import subprocess
+import sys
+
+jira_path = 'https://liaison-intl.atlassian.net/browse/'
+project_prefix = 'OUT'
+
+if (len(sys.argv) > 1):
+    ticket = sys.argv[1]
+else:
+    # assign from prompt
+    print('Jira ticket: ')
+    ticket = sys.stdin.readline().rstrip()
+
+try:
+    if (isinstance(int(ticket), (int)) and not isinstance(int(ticket), bool)):
+        ticket = project_prefix + '-' + ticket
+except:
+    pass
+
+ticket = str(ticket)
+output = '[' + ticket + '](' + jira_path + ticket + ')'
+
+subprocess.run('pbcopy', text=True, input=output)
+print('\n' + output + '\n')
