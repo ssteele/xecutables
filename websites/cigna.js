4efdfc2f25fdb2bcd78626afab0716f298d0cf9c
Steve Steele <steven.harville.steele@gmail.com>
2025-03-17 11:29:54 -0500

    Updates

---
+++ websites/cigna.js
@@ -0,0 +1,6 @@
+
+// add all suggested friends
+$$('.v-list-item-action .v-icon-button').forEach(e => e.click());
+
+// dismiss accepted friend requests
+$$('.vpg-t-btn.vpg-button-large').forEach(e => e.click());
