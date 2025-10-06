cb03deb3f69f9e44fa5cf62436f5d9c5c628a05e
Steve Steele <steven.harville.steele@gmail.com>
2025-10-05 20:38:49 -0500

    Updates

--- websites/cigna.js
+++ websites/cigna.js
@@ -11,3 +11,10 @@ $$('.v-list-item-action .v-icon-button').forEach(e => e.click());
 
 // invite everyone to a challenge
 $$('#fa--list-item-button').forEach(e => e.click())
+
+
+
+
+// new challenge
+// https://app.personifyhealth.com/#/steps-leaderboard
+// https://app.personifyhealth.com/#/featuredchallenge/82927
